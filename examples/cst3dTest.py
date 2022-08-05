8312b8ea7bdf3a5ea5e86ef989730c9292207fee
Andrew Thomas <anthomas@iastate.edu>
2022-08-05 18:59:43 -0500

    Fixed 3d design gui

--- examples/cst3dTest.py
+++ examples/cst3dTest.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 #Constants
 rootChord = 1.0
 tipChord = 0.25 * rootChord
-span = 4.0 * rootChord
 totalSweep = np.radians(20.0)
 totalShear = 0.25
 totalTwist = np.radians(10.0)
@@ -24,23 +23,35 @@ tri = Delaunay(np.array([psi,eta]).T)
 ##########################################################################
 
 ##Plot Airfoil Example:
-# span = 1.0
-# upper = CSTAirfoil3D(psiEtaZeta, refSpan=span, chordCoeffs=[rootChord])
-# lower = CSTAirfoil3D(psiEtaZeta, refSpan=span, chordCoeffs=[rootChord], shapeScale=-1.0)
+span = 1.0
+upper = CSTAirfoil3D(psiEtaZeta, refSpan=span, chordCoeffs=[rootChord])
+lower = CSTAirfoil3D(psiEtaZeta, refSpan=span, chordCoeffs=[rootChord], shapeScale=-1.0)
+
+upper.setPsiEtaZeta(psi, eta)
+uCoords = upper.updateCoords()
+lower.setPsiEtaZeta(psi, eta)
+lCoords = lower.updateCoords()
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_trisurf(uCoords[:,0], uCoords[:,1], uCoords[:,2], triangles=tri.simplices, cmap=plt.cm.Spectral)
+ax.plot_trisurf(lCoords[:,0], lCoords[:,1], lCoords[:,2], triangles=tri.simplices, cmap=plt.cm.Spectral)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.axes.set_xlim3d(left=-span/2, right=span/2) 
+ax.axes.set_ylim3d(bottom=0, top=span) 
+ax.axes.set_zlim3d(bottom=-span/2, top=span/2)
+plt.show()
 
 ##########################################################################
 
 ##Plot Wing ex: ## Origin at LE of root chord
-
+span = 4.0 * rootChord
 upper = CSTWing3D(psiEtaZeta, refSpan=span, sweepCoeffs=[totalSweep], shearCoeffs=[totalShear], twistCoeffs=[totalTwist],
                   chordCoeffs=[rootChord, tipChord])
 lower = CSTWing3D(psiEtaZeta, refSpan=span, sweepCoeffs=[totalSweep], shearCoeffs=[totalShear], twistCoeffs=[totalTwist],
                   chordCoeffs=[rootChord, tipChord], shapeScale=-1.0)
-
-##########################################################################
-"""
-Uncomment this block for any tutorial above
-"""
 
 upper.setPsiEtaZeta(psi, eta)
 uCoords = upper.updateCoords()
@@ -62,6 +73,16 @@ plt.show()
 ##########################################################################
 
 #3d Design GUI Example
+
+upper = CSTWing3D(psiEtaZeta, refSpan=span, sweepCoeffs=[totalSweep], shearCoeffs=[totalShear], twistCoeffs=[totalTwist],
+                  chordCoeffs=[rootChord, tipChord])
+lower = CSTWing3D(psiEtaZeta, refSpan=span, sweepCoeffs=[totalSweep], shearCoeffs=[totalShear], twistCoeffs=[totalTwist],
+                  chordCoeffs=[rootChord, tipChord], shapeScale=-1.0)
+
+upper.setPsiEtaZeta(psi, eta)
+uCoords = upper.updateCoords()
+lower.setPsiEtaZeta(psi, eta)
+lCoords = lower.updateCoords()
 
 geo = GeoEx(surfaces=[upper, lower])
 p = PyrocDesign(geo,mode='3d')
