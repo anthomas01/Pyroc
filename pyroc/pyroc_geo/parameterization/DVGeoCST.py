Fixed derivatives pseudo-2d

pyroc/pyroc_geo/parameterization/DVGeoCST.py
@@ -39,10 +39,10 @@ class DVGeometryCST(DVGeometry):
 
         self.ptSetNames.append(ptName)
         self.zeroJacobians([ptName])
-        self.nPts[ptName] = None
 
         points = np.array(points).real.astype('d')
         self.points[ptName] = points
+        self.nPts[ptName] = len(points.flatten())
 
         # Ensure we project into the undeformed geometry
         if origConfig:
@@ -166,78 +166,43 @@ class DVGeometryCST(DVGeometry):
     def computeTotalJacobian(self, ptSetName, config=None):
         """Return the total point jacobian in CSR format since we
         need this for TACS"""
-        self.computeTotalJacobianFD(ptSetName, config=config)
     
-        # # Finalize the object, if not done yet
-        # self.finalize()
-        # self.curPtSet = ptSetName
+        # Finalize the object, if not done yet
+        self.finalize()
+        self.curPtSet = ptSetName
 
-        # if self.JT[ptSetName] is not None:
-        #     return
+        if self.JT[ptSetName] is not None:
+            return
 
-        # # compute the derivatives of the coefficients of this level wrt all of the design
-        # # variables at this level and all levels above
-        # J_temp = self.computeDVJacobian(config=config)
+        if self.nPts[ptSetName] is None:
+            coords0 = self.update(ptSetName, config=config).flatten()
+            self.nPts[ptSetName] = len(coords0.flatten())
 
-        # # Update dPtdCoef
-        # self.param.calcdPtdCoef(ptSetName)
+        NDV = self.getNDV()
+        if NDV > 0:
+            dvCounter = 0
+            totalJac = np.zeros((NDV, self.nPts[ptSetName]))
 
-        # # now get the derivative of the points for this level wrt the coefficients(dPtdCoef)
-        # if self.param.embeddedSurfaces[ptSetName].dPtdCoef is not None:
-        #     dPtdCoef = self.param.embeddedSurfaces[ptSetName].dPtdCoef.tocsr()
+            # Update dPtdGlobal
+            dPtdGlobal = self.computeGlobalJacobian(ptSetName, config)
+            if dPtdGlobal is not None:
+                dvCounter = self.getNDVGlobal()
+                totalJac[:dvCounter,:] = dPtdGlobal
 
-        #     # Do Sparse Mat-Mat multiplication and resort indices
-        #     if J_temp is not None:
-        #         print(J_temp.get_shape())
-        #         print(dPtdCoef.get_shape())
-        #         self.JT[ptSetName] = (J_temp.T * dPtdCoef.T).tocsr()
-        #         self.JT[ptSetName].sort_indices()
-        # else:
-        #     self.JT[ptSetName] = None
+            # Update dPtdLocal
+            self.param.calcdPtdCoef(ptSetName)
+            dPtdLocal = self.param.embeddedSurfaces[ptSetName].dPtdCoef.T
+            if dPtdLocal is not None:
+                totalJac[dvCounter:,:] = dPtdLocal
+        
+            self.JT[ptSetName] = totalJac.tocsr()
+            self.JT[ptSetName].sort_indices()
+
+        else:
+            self.JT[ptSetName] = None
 
     ### Internal Functions
 
     def finalize(self):
-        if self.finalized:
-            return
-        self.finalized = True
+        super().finalize()
         self.nCoefFull = sum([len(_) for _ in self.param.coef])
-
-    def localDVJacobian(self, config=None):
-        """
-        Return the derivative of the coefficients wrt the local design
-        variables
-        """
-
-        # This is relatively straight forward, since the matrix is
-        # entirely one's or zeros
-        nDV = self.getNDVLocal()
-        self.getDVOffsets()
-
-        if nDV != 0:
-            Jacobian = sparse.lil_matrix((3 * self.nCoefFull, self.nDV_T))
-
-            iDVLocal = self.nDVL_count
-            for key in self.DV_listLocal:
-                if (
-                    self.DV_listLocal[key].config is None
-                    or config is None
-                    or any(c0 == config for c0 in self.DV_listLocal[key].config)
-                ):
-
-                    self.DV_listLocal[key](self.param.coef, config)
-                    nVal = self.DV_listLocal[key].nVal
-                    for j in range(nVal):
-                        pt_dv = self.DV_listLocal[key].ind[j] # [paramNameIndex, index]
-                        shift = sum([len(self.param.coef[_]) for _ in range(pt_dv[0])])
-                        irow = 3 * (pt_dv[0] * shift + pt_dv[1])
-                        Jacobian[irow, iDVLocal] = 1.0
-                        Jacobian[irow+1, iDVLocal] = 1.0
-                        Jacobian[irow+2, iDVLocal] = 1.0
-                        iDVLocal += 1
-
-                else:
-                    iDVLocal += self.DV_listLocal[key].nVal
-        else:
-            Jacobian = None
-        return Jacobian

pyroc/pyroc_geo/parameterization/DVGeometry.py
@@ -13,7 +13,6 @@ class DVGeometry(object):
         self.DV_listLocal = OrderedDict()  # Local Design Variable List
 
         self.points = OrderedDict()
-        self.pointSets = OrderedDict()
         self.updated = {}
         self.ptSetNames = []
         self.name = name
@@ -515,22 +514,6 @@ class DVGeometry(object):
             i += dv.nVal
         return xsdict
 
-    def computeDVJacobian(self, config=None):
-        """
-        return J_temp for a given config
-        """
-
-        # This is the sparse jacobian for the local DVs that affect
-        # Control points directly.
-        J_local = self.localDVJacobian(config=config)
-
-        J_temp = None
-
-        if J_local is not None:
-            J_temp = sparse.lil_matrix(J_local)
-
-        return J_temp
-
     def computeTotalJacobian(self, ptSetName, config=None):
         """Return the total point jacobian in CSR format since we
         need this for TACS"""
@@ -624,7 +607,9 @@ class DVGeometry(object):
     #Helper Functions
 
     def finalize(self):
-        pass
+        if self.finalized:
+            return
+        self.finalized = True
 
     def getNDV(self):
         """
@@ -672,8 +657,37 @@ class DVGeometry(object):
 
         return self.nDVG_count, self.nDVL_count
 
-    def localDVJacobian(self, config=None):
-        pass
+    def computeGlobalJacobian(self, ptSetName, config=None, h=1e-6):
+        coords0 = self.update(ptSetName, config=config).flatten()
+
+        if self.nPts[ptSetName] is None:
+            self.nPts[ptSetName] = len(coords0.flatten())
+
+        DVGlobalCount = self.getNDVGlobal()
+
+        if DVGlobalCount > 0:
+
+            globalJac = np.zeros((DVGlobalCount, self.nPts[ptSetName]))
+
+            for key in self.DV_listGlobal:
+                for j in range(self.DV_listGlobal[key].nVal):
+
+                    refVal = self.DV_listGlobal[key].value[j]
+
+                    self.DV_listGlobal[key].value[j] += h
+
+                    coordsph = self.update(ptSetName, config=config).flatten()
+
+                    deriv = (coordsph - coords0) / h
+                    globalJac[DVGlobalCount, :] = deriv
+
+                    DVGlobalCount += 1
+                    self.DV_listGlobal[key].value[j] = refVal
+
+            return globalJac
+
+        else:
+            return None
 
     def computeTotalJacobianFD(self, ptSetName, config=None, h=1e-6):
         """This function takes the total derivative of an objective,
@@ -696,7 +710,7 @@ class DVGeometry(object):
 
         DVGlobalCount, DVLocalCount = self.getDVOffsets()
 
-        self.JT[ptSetName] = np.zeros([self.nDV_T, self.nPts[ptSetName]])
+        self.JT[ptSetName] = np.zeros((self.nDV_T, self.nPts[ptSetName]))
 
         for key in self.DV_listGlobal:
             for j in range(self.DV_listGlobal[key].nVal):
@@ -787,8 +801,8 @@ class DVGeometry(object):
                     relErr = (deriv[ii] - Jac[DVCountGlob, ii]) / (1e-16 + Jac[DVCountGlob, ii])
                     absErr = deriv[ii] - Jac[DVCountGlob, ii]
 
-                    if abs(relErr) > h * 10 and abs(absErr) > h * 10:
-                        print(ii, deriv[ii], Jac[DVCountGlob, ii], relErr, absErr)
+                    #if abs(relErr) > h * 10 and abs(absErr) > h * 10:
+                    print(ii, deriv[ii], Jac[DVCountGlob, ii], relErr, absErr)
 
                 DVCountGlob += 1
                 self.DV_listGlobal[key].value[j] = refVal
@@ -811,8 +825,8 @@ class DVGeometry(object):
                     relErr = (deriv[ii] - Jac[DVCountLoc, ii]) / (1e-16 + Jac[DVCountLoc, ii])
                     absErr = deriv[ii] - Jac[DVCountLoc, ii]
 
-                    if abs(relErr) > h and abs(absErr) > h:
-                        print(ii, deriv[ii], Jac[DVCountLoc, ii], relErr, absErr)
+                    #if abs(relErr) > h and abs(absErr) > h:
+                    print(ii, deriv[ii], Jac[DVCountLoc, ii], relErr, absErr)
 
                 DVCountLoc += 1
                 self.DV_listLocal[key].value[j] = refVal

pyroc/pyroc_utils/cst3d.py
@@ -93,9 +93,8 @@ class CST3DParam(object):
         shapeScale=-1.0 can be used for a -z surface initialization
         By default, 1.0
 
-    - TODO Multiple sections - Higher level of abstraction?
+    - TODO Fix Derivative Implementation -- default and airfoil complete
     - TODO Overall abstraction/simplification, bugfixes
-    - TODO Use internal derivatives with scipy optimization
     """
 
     def __init__(self, surface, csClassFunc=None, csModFunc=None, spanClassFunc=None, spanModFunc=None, refLenFunc=None,
@@ -467,7 +466,7 @@ class CST3DParam(object):
             return self.calcXY2Z(xyVals[:,0], xyVals[:,1])
         def jac(xyVals, *coeffs):
             surf = np.vstack([xyVals[:,0], xyVals[:,1], surface(xyVals, *coeffs)]).T
-            return self.calcJacobian(surf)[2::3,2::3]
+            return self.calcJacobian(surf)[2::3,:]
         #TODO Fix warning for cov params being inf in certain conditions
         coeffs,cov = scp.curve_fit(surface,coords[:,0:2],coords[:,2],np.atleast_1d(self.getCoeffs()),jac=jac)
         self.updateCoeffs(coeffs)
@@ -652,22 +651,23 @@ class CSTAirfoil3D(CST3DParam):
         return np.vstack([self.csGeo.calcDeriv(psiEtaZeta[:,0], h), np.zeros(len(psiEtaZeta))]).T
 
     #TODO make this more efficient
-    #Calculate dXYZdCoeffs Jacobian (Analytical)
-    #dXYZdCoeff = dXYZdPsiEtaZeta * dPsiEtaZetadCoeff
+    #Calculate dUVWdCoeffs Jacobian (Analytical)
+    #dUVWdCoeffs = dUVWdXYZ * dXYZdPsiEtaZeta * dPsiEtaZetadCoeffs
     def calcJacobian(self, surface, paramScale=None, h=1e-8):
         nPts = len(surface)
         nCoeffs = len(self.getCoeffs())
         if nCoeffs>0 and nPts>0:
             psiEtaZeta = self.coords2PsiEtaZeta(surface)
-            ptsJac = self._calcPtsJacobian(psiEtaZeta, h)
-            paramsJac = self.calcParamsJacobian(psiEtaZeta, paramScale, h)
-            totalJac = np.zeros((3*nPts, 3*nCoeffs))
+            ptsJac = self._calcPtsJacobian(psiEtaZeta, h) #dXYZdPsiEtaZeta [3*NPts x 3]
+            transJac = self._calcTransformJacobian(psiEtaZeta, h) #dUVWdXYZ [3*NPts x 3]
+            paramsJac = self.calcParamsJacobian(psiEtaZeta, paramScale, h) #dPsiEtaZetadCoeffs [3*NPts x NDVs]
+            totalJac = np.zeros((3*nPts, nCoeffs))
+
             for _ in range(nPts):
-                ptJac = ptsJac[3*_:3*(_+1),:]
-                for __ in range(nCoeffs):
-                    paramJac = paramsJac[3*_:3*(_+1), 3*__:3*(__+1)]
-                    dXYZdCoeffVec = ptJac @ np.diagonal(paramJac)
-                    totalJac[3*_:3*(_+1), 3*__:3*(__+1)] = np.diag(dXYZdCoeffVec)
+                #dUVWdCoeffs = dUVWdXYZ * dXYZdPsiEtaZeta * dPsiEtaZetadCoeffs
+                totalPtJac = transJac[3*_:3*(_+1),:] @ ptsJac[3*_:3*(_+1),:] @ paramsJac[3*_:3*(_+1),:]
+                totalJac[3*_:3*(_+1), :] = totalPtJac
+
         else:
             totalJac = None
         return totalJac
@@ -706,7 +706,8 @@ class CSTAirfoil3D(CST3DParam):
         nCoeffs = len(self.chordCoeffs)
         if nCoeffs>0:
             refChordJac = np.zeros((len(psiEtaZeta.flatten()),nCoeffs))
-            refChordJac[2::3, :] = -self.shapeOffsets[0]*psiEtaZeta[:,0]*np.power(self.chordCoeffs[0],-2.0)
+            dZetadChord = -self.shapeOffsets[0]*psiEtaZeta[:,0]*np.power(self.chordCoeffs[0],-2.0)
+            refChordJac[2::3,:] = np.atleast_2d(dZetadChord).T
         else:
             refChordJac = None
         return refChordJac
@@ -729,24 +730,19 @@ class CSTAirfoil3D(CST3DParam):
     # i.e [dV/dCoeff] = [dV/dX dV/dY dV/dZ]*[dY/dCoeff]
     #     [dW/dCoeff]   [dW/dX dW/dY dW/dZ] [dZ/dCoeff]
     def _calcTransformJacobian(self, surface, h=1e-8):
-        transformJac = np.zeros((len(surface.flatten()),3))
-        for _ in range(len(surface)):
-            #For a linear transformation, this is just the transpose of the reference axes
-            transformJac[3*_:3*(_+1),:] = self.refAxes.T
+        transformJac = self._calcTransformMatrix(surface)
         return transformJac
 
     #dXYZdPsiEtaZeta (analytical)
+    #     [dX/dCoeff]   [dX/dPsi dX/dEta dX/dZeta] [dPsi/dCoeff]
+    # i.e [dY/dCoeff] = [dY/dPsi dY/dEta dY/dZeta]*[dEta/dCoeff]
+    #     [dZ/dCoeff]   [dZ/dPsi dZ/dEta dZ/dZeta] [dZeta/dCoeff]
     def _calcPtsJacobian(self, psiEtaZeta, h=1e-8):
         ptsJac = np.zeros((len(psiEtaZeta.flatten()), 3))
         #transformation matrix from normalizations
         ptsJac[0::3, 0] = self.refLen(psiEtaZeta, self.chordCoeffs)
         ptsJac[1::3, 1] = self.refSpan
         ptsJac[2::3, 2] = self.refLen(psiEtaZeta, self.chordCoeffs)
-        #Pre-multiply by transformation from coordinate axes
-        surface = self.calcCoords(psiEtaZeta)
-        transformJac = self._calcTransformJacobian(surface, h)
-        for _ in range(len(psiEtaZeta)):
-            ptsJac[3*_:3*(_+1),:] = transformJac[3*_:3*(_+1),:] @ ptsJac[3*_:3*(_+1),:]
         return ptsJac
 
 #################################################################################################

pyroc/pyroc_utils/cstMulti.py
@@ -358,7 +358,7 @@ class EmbeddedSurface(object):
             for _ in coordinateIndices:
                 j = 0
                 for __ in coefMap[paramName]:
-                    dPtdCoef[3*_:3*(_+1), 3*__:3*(__+1)] = choppeddPtdCoef[3*i:3*(i+1), 3*j:3*(j+1)]
+                    dPtdCoef[3*_:3*(_+1), __] = choppeddPtdCoef[3*i:3*(i+1), j]
                     j+=1
                 i+=1
 
@@ -492,7 +492,7 @@ class EmbeddedParameterization(object):
 
     def calcdPtdCoef(self, coordinates, **kwargs):
         nCoeff = len(self.param.getCoeffs())
-        paramScale = np.hstack([np.vstack([np.eye(3) for _ in range(len(coordinates))]) for _ in range(nCoeff)])
+        paramScale = np.ones((len(coordinates.flatten()),nCoeff))
 
         for connectionName in self.connectionsDict:
             connection = self.connectionsDict[connectionName]
@@ -514,8 +514,8 @@ class EmbeddedParameterization(object):
                     jac1 = self.param.calcJacobian(np.atleast_2d(psiEtaZeta))
                     jac2 = connection['connectingParam'].param.calcJacobian(np.atleast_2d(psiEtaZeta))
 
-                    duplicatedZeta1 = jac1[2+3*totalDuplicatePsiEtaIndices, 2::3]
-                    duplicatedZeta2 = jac2[2+3*totalDuplicatePsiEtaIndices, 2::3]
+                    duplicatedZeta1 = jac1[2+3*totalDuplicatePsiEtaIndices,:]
+                    duplicatedZeta2 = jac2[2+3*totalDuplicatePsiEtaIndices,:]
 
                     for _ in range(nCoeff):
                         zeroTotalDuplicateIndices = totalDuplicatePsiEtaIndices[duplicatedZeta1[:,_]==0]
@@ -527,13 +527,12 @@ class EmbeddedParameterization(object):
                         _totalCountsDuplicates = totalCountsDuplicates[nonzeroTotalDuplicateIndices]
 
                         #Scale zeta values for duplicate psi,eta
-                        paramScale[2+3*zeroTotalDuplicateIndices,2+3*_] = np.zeros_like(zeroTotalDuplicateIndices)
-                        paramScale[2+3*nonzeroTotalDuplicateIndices,2+3*_] = (nonzeroDuplicatedZeta1 - (nonzeroDuplicatedZeta1 -
-                                                                               nonzeroDuplicatedZeta2) * (_countsDuplicates - 1) /
-                                                                              (_totalCountsDuplicates - 1)) / nonzeroDuplicatedZeta1
+                        paramScale[2+3*zeroTotalDuplicateIndices,_] = np.zeros_like(zeroTotalDuplicateIndices)
+                        paramScale[2+3*nonzeroTotalDuplicateIndices,_] = (nonzeroDuplicatedZeta1 - (nonzeroDuplicatedZeta1 -
+                                                                          nonzeroDuplicatedZeta2) * (_countsDuplicates - 1) /
+                                                                          (_totalCountsDuplicates - 1)) / nonzeroDuplicatedZeta1
                 
         dPtdCoef = self.param.calcJacobian(np.atleast_2d(coordinates), paramScale=paramScale)
-
         return dPtdCoef
 
 