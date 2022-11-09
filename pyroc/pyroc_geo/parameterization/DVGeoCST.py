Progress on derivative fix, connection implementation incorrect?

pyroc/pyroc_geo/parameterization/DVGeoCST.py
@@ -180,23 +180,23 @@ class DVGeometryCST(DVGeometry):
 
         NDV = self.getNDV()
         if NDV > 0:
-            dvCounter = 0
-            totalJac = np.zeros((NDV, self.nPts[ptSetName]))
+            totalJac = None
 
             # Update dPtdGlobal
             dPtdGlobal = self.computeGlobalJacobian(ptSetName, config)
             if dPtdGlobal is not None:
-                dvCounter = self.getNDVGlobal()
-                totalJac[:dvCounter,:] = dPtdGlobal
+                totalJac = sparse.csr_matrix(dPtdGlobal)
 
             # Update dPtdLocal
-            self.param.calcdPtdCoef(ptSetName)
-            dPtdLocal = self.param.embeddedSurfaces[ptSetName].dPtdCoef.T
+            dPtdLocal = sparse.csr_matrix(self.computeLocalJacobian(ptSetName, config))
             if dPtdLocal is not None:
-                totalJac[dvCounter:,:] = dPtdLocal
+                if totalJac is None:
+                    totalJac = dPtdLocal
+                else:
+                    totalJac = sparse.vstack((totalJac, dPtdLocal))
         
-            self.JT[ptSetName] = totalJac.tocsr()
-            self.JT[ptSetName].sort_indices()
+            self.JT[ptSetName] = totalJac
+            #self.JT[ptSetName].sort_indices()
 
         else:
             self.JT[ptSetName] = None
@@ -206,3 +206,20 @@ class DVGeometryCST(DVGeometry):
     def finalize(self):
         super().finalize()
         self.nCoefFull = sum([len(_) for _ in self.param.coef])
+
+    def computeLocalJacobian(self, ptSetName, config=None):
+        #Update
+        self.param.calcdPtdCoef(ptSetName)
+        totaldPtdLocal = self.param.embeddedSurfaces[ptSetName].dPtdCoef.T
+
+        #Only get indices needed by design variables
+        dPtdLocal = np.zeros((self.getNDVLocal(), self.nPts[ptSetName]))
+        localCounter = 0
+        for key in self.DV_listLocal:
+            indices = self.DV_listLocal[key].ind
+            for j in range(self.DV_listLocal[key].nVal):
+                dvBuf = sum([len(self.param.coef[indices[i,0]]) for i in range(indices[j,0])])
+                dPtdLocal[localCounter, :] = totaldPtdLocal[indices[j,1]+dvBuf, :]
+                localCounter+=1
+
+        return dPtdLocal

pyroc/pyroc_geo/parameterization/DVGeometry.py
@@ -710,7 +710,7 @@ class DVGeometry(object):
 
         DVGlobalCount, DVLocalCount = self.getDVOffsets()
 
-        self.JT[ptSetName] = np.zeros((self.nDV_T, self.nPts[ptSetName]))
+        totalJacFD = np.zeros((self.nDV_T, self.nPts[ptSetName]))
 
         for key in self.DV_listGlobal:
             for j in range(self.DV_listGlobal[key].nVal):
@@ -722,7 +722,7 @@ class DVGeometry(object):
                 coordsph = self.update(ptSetName, config=config).flatten()
 
                 deriv = (coordsph - coords0) / h
-                self.JT[ptSetName][DVGlobalCount, :] = deriv
+                totalJacFD[DVGlobalCount, :] = deriv
 
                 DVGlobalCount += 1
                 self.DV_listGlobal[key].value[j] = refVal
@@ -736,10 +736,12 @@ class DVGeometry(object):
                 coordsph = self.update(ptSetName, config=config).flatten()
 
                 deriv = (coordsph - coords0) / h
-                self.JT[ptSetName][DVLocalCount, :] = deriv
+                totalJacFD[DVLocalCount, :] = deriv
 
                 DVLocalCount += 1
                 self.DV_listLocal[key].value[j] = refVal
+
+        self.JT[ptSetName] = sparse.csr_matrix(totalJacFD)
     
     def printDesignVariables(self):
         """
@@ -766,7 +768,7 @@ class DVGeometry(object):
         """
 
         print("Computing Analytic Jacobian...")
-        self.zeroJacobians(ptSetName)
+        self.zeroJacobians([ptSetName])
         self.computeTotalJacobian(ptSetName)
 
         Jac = copy.deepcopy(self.JT[ptSetName])
@@ -801,8 +803,8 @@ class DVGeometry(object):
                     relErr = (deriv[ii] - Jac[DVCountGlob, ii]) / (1e-16 + Jac[DVCountGlob, ii])
                     absErr = deriv[ii] - Jac[DVCountGlob, ii]
 
-                    #if abs(relErr) > h * 10 and abs(absErr) > h * 10:
-                    print(ii, deriv[ii], Jac[DVCountGlob, ii], relErr, absErr)
+                    if abs(relErr) > h * 10 and abs(absErr) > h * 10:
+                        print(ii, deriv[ii], Jac[DVCountGlob, ii], relErr, absErr)
 
                 DVCountGlob += 1
                 self.DV_listGlobal[key].value[j] = refVal
@@ -825,8 +827,8 @@ class DVGeometry(object):
                     relErr = (deriv[ii] - Jac[DVCountLoc, ii]) / (1e-16 + Jac[DVCountLoc, ii])
                     absErr = deriv[ii] - Jac[DVCountLoc, ii]
 
-                    #if abs(relErr) > h and abs(absErr) > h:
-                    print(ii, deriv[ii], Jac[DVCountLoc, ii], relErr, absErr)
+                    if ii in [17,20,23,26,29,32,8]:
+                        print(ii, deriv[ii], Jac[DVCountLoc, ii], relErr, absErr)
 
                 DVCountLoc += 1
                 self.DV_listLocal[key].value[j] = refVal

pyroc/pyroc_utils/cst2d.py
@@ -295,6 +295,7 @@ class CSTAirfoil2D(CST2DParam):
         coeffs = coeffs[0]
         return np.power(psiVals,coeffs[0])*np.power(1-psiVals,coeffs[1])
 
+    #TODO - Check
     #LE Radius Functions
     def updateLERadius(self, r):
         self.shapeCoeffs[0] = np.sqrt(2*r/self.refLen)
@@ -304,6 +305,7 @@ class CSTAirfoil2D(CST2DParam):
         r = 0.5*self.refLen*np.power(self.shapeCoeffs[0],2)
         return r
 
+    #TODO - Check
     #Boattail Angle Function
     def updateTEAngle(self, beta):
         self.shapeCoeffs[-1] = np.tan(beta)+self.shapeOffset
@@ -340,7 +342,6 @@ class CSTAirfoil2D(CST2DParam):
 
     #dZetadClassCoeff - analytic, class func is known
     def _calcClassJacobian(self, psiVals, h=1e-8):
-        n1,n2 = self.classCoeffs
         dZetadN1 = np.log(psiVals)*self.classFunc(psiVals,self.classCoeffs)*self.shapeFunc(psiVals,self.shapeCoeffs)
         dZetadN2 = np.log(1-psiVals)*self.classFunc(psiVals,self.classCoeffs)*self.shapeFunc(psiVals,self.shapeCoeffs)
         dZetadClass = np.vstack([dZetadN1,dZetadN2]).T

pyroc/pyroc_utils/cst3d.py
@@ -631,8 +631,8 @@ class CSTAirfoil3D(CST3DParam):
 
     #Calculate psi,eta,zeta from surface
     def coords2PsiEtaZeta(self, surface):
-        psiEtaZeta =  super().coords2PsiEtaZeta(surface)
-        psiEtaZeta[:,0][psiEtaZeta[:,0]==0.0] = 1e-12
+        psiEtaZeta = super().coords2PsiEtaZeta(surface)
+        psiEtaZeta[:,0][psiEtaZeta[:,0]==0.0] = 1e-16
         return psiEtaZeta
 
     def calcXZ2Y(self, xVals, zVals):
@@ -665,8 +665,7 @@ class CSTAirfoil3D(CST3DParam):
 
             for _ in range(nPts):
                 #dUVWdCoeffs = dUVWdXYZ * dXYZdPsiEtaZeta * dPsiEtaZetadCoeffs
-                totalPtJac = transJac[3*_:3*(_+1),:] @ ptsJac[3*_:3*(_+1),:] @ paramsJac[3*_:3*(_+1),:]
-                totalJac[3*_:3*(_+1), :] = totalPtJac
+                totalJac[3*_:3*(_+1), :] = transJac[3*_:3*(_+1),:] @ ptsJac[3*_:3*(_+1),:] @ paramsJac[3*_:3*(_+1),:]
 
         else:
             totalJac = None
@@ -678,7 +677,7 @@ class CSTAirfoil3D(CST3DParam):
         #Implement param scale for above level connections
         #Param scale must be (N,3) where N is length of paramsJac
         if paramScale is not None:
-            paramsJac = paramsJac * paramScale
+            paramsJac = np.multiply(paramsJac,paramScale)
         return paramsJac
 
     #dPsiEtaZetadCsClassCoeffs (Analytical)

pyroc/pyroc_utils/cstMulti.py
@@ -15,12 +15,14 @@ class CSTMultiParam(object):
         self.embeddedParams = OrderedDict()
         #Refit when adding new coords?
         self.fit = False
+        self.fitIter = 2
+        self.sigFigs = 8 # Sig figs for fitting connections
 
     def attachPoints(self, coordinates, ptSetName, embeddedParams=None, parameterization=False):
         #Project points to surface/CST geom object if some were passed in
         if coordinates is not None:
             embeddedParams = embeddedParams if embeddedParams is not None else self.embeddedParams
-            self.embeddedSurfaces[ptSetName] = EmbeddedSurface(coordinates, embeddedParams, self.fit, parameterization=parameterization)
+            self.embeddedSurfaces[ptSetName] = EmbeddedSurface(coordinates, embeddedParams, fit=self.fit, fitIter=self.fitIter, parameterization=parameterization, sig=self.sigFigs)
 
     def attachParameterization(self, parameterization, ptSetName, embeddedParams=None):
         #Project points to surface/CST geom object if some were passed in
@@ -156,7 +158,7 @@ class CSTMultiParam(object):
 class EmbeddedSurface(object):
     #Class for managing multiple CST surfaces
 
-    def __init__(self, coordinates, embeddedParams, fit=False, fitIter=1, parameterization=False):
+    def __init__(self, coordinates, embeddedParams, fit=False, fitIter=2, parameterization=False, sig=8):
         self.embeddedParams = embeddedParams
         self.N = len(coordinates)
         self.connectionsDict = OrderedDict()
@@ -168,7 +170,7 @@ class EmbeddedSurface(object):
             #Determine which embedded parameterization object each coordinate belongs to
             self.paramMap = self.mapParameterization2Params(self.parameterization) #list
 
-            self.initializeParameterizationConnections()
+            self.initializeParameterizationConnections(sig)
 
             self.coordinates = self.parameterization
 
@@ -185,7 +187,7 @@ class EmbeddedSurface(object):
                 #Group coordinates by parameterization and apply fit if True
                 self.parameterization = np.round(self.fitParams(fit), decimals=15)
 
-                self.initializeParameterizationConnections()
+                self.initializeParameterizationConnections(sig)
     
     #TODO Make these more efficient
     def mapCoords2Params(self):
@@ -232,7 +234,7 @@ class EmbeddedSurface(object):
                     closest = d2
         return paramMap
 
-    def initializeParameterizationConnections(self):
+    def initializeParameterizationConnections(self, decTol=8):
         '''
         Determines connection parameters for point set
         '''
@@ -249,17 +251,17 @@ class EmbeddedSurface(object):
                     dtypeCounts = int if connection['linear'] else float
 
                     #Find duplicate psi,eta values for entire point set
-                    psiEtaVals = self.parameterization[:,0:2]
+                    psiEtaVals = np.round(self.parameterization[:,0:2],decTol)
                     uniquePsiEta, uniquePsiEtaIndices, countsPsiEta = np.unique(psiEtaVals, return_index=True, return_counts=True, axis=0)
                     duplicatePsiEtaIndices = [i for i in range(len(self.parameterization)) if i not in uniquePsiEtaIndices]
                     uniqueDuplicatePsiEta = uniquePsiEta[countsPsiEta>1]
-                    totalDuplicatePsiEtaIndices = np.append(uniquePsiEtaIndices[np.where(countsPsiEta>1)[0]], duplicatePsiEtaIndices)
+                    totalDuplicatePsiEtaIndices = np.sort(np.append(uniquePsiEtaIndices[np.where(countsPsiEta>1)[0]], duplicatePsiEtaIndices))
                     totalCountsDuplicates = np.ones(len(self.parameterization), dtype=int)
                     countsDuplicates = np.ones(len(self.parameterization), dtype=dtypeCounts)
 
                     for _ in range(len(totalDuplicatePsiEtaIndices)):
                         index = totalDuplicatePsiEtaIndices[_]
-                        psiEtaZeta = self.parameterization[index]
+                        psiEtaZeta = np.round(self.parameterization[index],decTol)
                         
                         uniquePsiIndices = np.where(uniquePsiEta[:,0]==psiEtaZeta[0])[0]
                         uniqueEtaIndices = np.where(uniquePsiEta[:,1]==psiEtaZeta[1])[0]
@@ -290,7 +292,7 @@ class EmbeddedSurface(object):
 
                         #Issue with points rounding to wrong place in linear?
                         if connection['linear']:
-                            countsDuplicates[thisPsiEtaIndices] = np.round(n1, decimals=0)
+                            countsDuplicates[thisPsiEtaIndices] = np.round(n1,0) #FIXME
                         else:
                             countsDuplicates[thisPsiEtaIndices] = n1
 
@@ -332,12 +334,12 @@ class EmbeddedSurface(object):
         #find nCoef and coefMap
         nCoef = 0
         coefMap = {}
-        for paramName in np.unique(self.paramMap):
+        for paramName in self.embeddedParams:
             paramCoeffs = self.embeddedParams[paramName].coeffs
             coefMap[paramName] = [_+nCoef for _ in range(len(paramCoeffs))]
             nCoef += len(paramCoeffs)
 
-        #size of dPtdCoef will be 3*Npts x 3*Ncoef
+        #size of dPtdCoef will be 3*Npts x Ncoef
         dPtdCoef = np.zeros((3*nPts, nCoef))
 
         for paramName in np.unique(self.paramMap):
@@ -362,8 +364,7 @@ class EmbeddedSurface(object):
                     j+=1
                 i+=1
 
-        #Finally, store variable as sparse matrix
-        self.dPtdCoef = sparse.csr_matrix(dPtdCoef)
+        self.dPtdCoef = dPtdCoef
 
     #Fit parameters for all parameterizations used by this pointset
     def fitParams(self, performFit=False):
@@ -458,12 +459,9 @@ class EmbeddedParameterization(object):
                 totalCountsDuplicates = kwargs['totalCountsDuplicates'] if 'totalCountsDuplicates' in kwargs else np.ones(len(psiEtaZeta))
 
                 #Find duplicate psi,eta values
-                psiEtaVals = psiEtaZeta[:,0:2]
-                uniquePsiEta, uniquePsiEtaIndices, countsPsiEta = np.unique(psiEtaVals, return_index=True, return_counts=True, axis=0)
-                duplicatePsiEtaIndices = np.array([i for i in range(len(psiEtaZeta)) if i not in uniquePsiEtaIndices])
-                totalDuplicatePsiEtaIndices = np.append(uniquePsiEtaIndices[np.where(countsPsiEta>1)[0]], duplicatePsiEtaIndices)
+                totalDuplicatePsiEtaIndices = np.where(totalCountsDuplicates>1)[0]
 
-                if len(totalDuplicatePsiEtaIndices)>0 and np.all(totalCountsDuplicates[duplicatePsiEtaIndices]>1):
+                if len(totalDuplicatePsiEtaIndices)>0 and np.all(totalCountsDuplicates[totalDuplicatePsiEtaIndices]>1):
                     duplicatePsiEtaZeta = psiEtaZeta[totalDuplicatePsiEtaIndices]
                     duplicateZeta1 = self.param.calcZeta(np.atleast_2d(duplicatePsiEtaZeta))
                     duplicateZeta2 = connection['connectingParam'].param.calcZeta(np.atleast_2d(duplicatePsiEtaZeta))
@@ -485,10 +483,10 @@ class EmbeddedParameterization(object):
                     zetaVals = self.param.calcZeta(np.atleast_2d(psiEtaZeta), zetaScale=zetaScale)
                     psiEtaZeta_ = psiEtaZeta
                     psiEtaZeta_[:,2] = zetaVals
-
+ 
         zetaVals = self.param.calcZeta(np.atleast_2d(psiEtaZeta), zetaScale=zetaScale)
-
         return zetaVals
+
 
     def calcdPtdCoef(self, coordinates, **kwargs):
         nCoeff = len(self.param.getCoeffs())
@@ -504,34 +502,34 @@ class EmbeddedParameterization(object):
                 totalCountsDuplicates = kwargs['totalCountsDuplicates'] if 'totalCountsDuplicates' in kwargs else np.ones(len(coordinates))
 
                 #Find duplicate psi,eta values
-                psiEtaZeta = self.calcParameterization(coordinates)
-                psiEtaVals = psiEtaZeta[:,0:2]
-                uniquePsiEta, uniquePsiEtaIndices, countsPsiEta = np.unique(psiEtaVals, return_index=True, return_counts=True, axis=0)
-                duplicatePsiEtaIndices = np.array([i for i in range(len(psiEtaZeta)) if i not in uniquePsiEtaIndices])
-                totalDuplicatePsiEtaIndices = np.append(uniquePsiEtaIndices[np.where(countsPsiEta>1)[0]], duplicatePsiEtaIndices)
+                totalDuplicatePsiEtaIndices = np.where(totalCountsDuplicates>1)[0]
 
                 if len(totalDuplicatePsiEtaIndices)>0 and np.all(totalCountsDuplicates[totalDuplicatePsiEtaIndices]>1):
-                    jac1 = self.param.calcJacobian(np.atleast_2d(psiEtaZeta))
-                    jac2 = connection['connectingParam'].param.calcJacobian(np.atleast_2d(psiEtaZeta))
+                    siEtaZeta = self.calcParameterization(coordinates)[totalDuplicatePsiEtaIndices]
+                    jac1 = self.param.calcJacobian(np.atleast_2d(siEtaZeta))
+                    jac2 = connection['connectingParam'].param.calcJacobian(np.atleast_2d(siEtaZeta))
 
-                    duplicatedZeta1 = jac1[2+3*totalDuplicatePsiEtaIndices,:]
-                    duplicatedZeta2 = jac2[2+3*totalDuplicatePsiEtaIndices,:]
+                    duplicatedZeta1 = jac1[2::3,:]
+                    duplicatedZeta2 = jac2[2::3,:]
 
                     for _ in range(nCoeff):
                         zeroTotalDuplicateIndices = totalDuplicatePsiEtaIndices[duplicatedZeta1[:,_]==0]
                         nonzeroTotalDuplicateIndices = totalDuplicatePsiEtaIndices[duplicatedZeta1[:,_]!=0]
 
                         nonzeroDuplicatedZeta1 = duplicatedZeta1[duplicatedZeta1[:,_]!=0,_]
-                        nonzeroDuplicatedZeta2 = duplicatedZeta2[duplicatedZeta1[:,_]!=0,_]
+                        nonzeroDuplicatedZeta2 = duplicatedZeta2[duplicatedZeta2[:,_]!=0,_]
+
                         _countsDuplicates = countsDuplicates[nonzeroTotalDuplicateIndices]
                         _totalCountsDuplicates = totalCountsDuplicates[nonzeroTotalDuplicateIndices]
 
                         #Scale zeta values for duplicate psi,eta
+                        #TODO Change implementation?
                         paramScale[2+3*zeroTotalDuplicateIndices,_] = np.zeros_like(zeroTotalDuplicateIndices)
                         paramScale[2+3*nonzeroTotalDuplicateIndices,_] = (nonzeroDuplicatedZeta1 - (nonzeroDuplicatedZeta1 -
                                                                           nonzeroDuplicatedZeta2) * (_countsDuplicates - 1) /
                                                                           (_totalCountsDuplicates - 1)) / nonzeroDuplicatedZeta1
-                
+
+        paramScale = np.ones((len(coordinates.flatten()),nCoeff))
         dPtdCoef = self.param.calcJacobian(np.atleast_2d(coordinates), paramScale=paramScale)
         return dPtdCoef
 

pyroc/pyroc_utils/miscCst.py
@@ -24,8 +24,8 @@ def bernstein1DDeriv(psiVals, n, h=1e-8):
     orderRange = np.arange(n+1) #binary coefficient constants
     binCoeffs = binaryCoefficients(n)
     if isScalar(psiVals):
-        terms = binCoeffs*(orderRange*np.power(psiVals,orderRange-1)*np.power(1-psiVals,n-orderRange) -
-                           (n-orderRange)*np.power(psiVals, orderRange)*np.power(1-psiVals,n-orderRange-1))
+        terms = binCoeffs * (orderRange*np.power(psiVals,orderRange-1)*np.power(1-psiVals,n-orderRange) -
+                             (n-orderRange)*np.power(psiVals,orderRange)*np.power(1-psiVals,n-orderRange-1))
     else:
         terms = []
         for psi in psiVals:
@@ -35,7 +35,7 @@ def bernstein1DDeriv(psiVals, n, h=1e-8):
 
 #dBernsteindShapeCoeffs
 def bernstein1DJacobian(psiVals, n, h=1e-8):
-    if n+1>0:
+    if n>=0:
         binCoeffs = binaryCoefficients(n)
         bernsteinJac = np.zeros((len(psiVals),n+1))
         for _ in range(n+1):